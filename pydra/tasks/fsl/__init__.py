9521d07696c20d6000cfdcc9bc13102468e9b763
Ghislain Vaillant <ghislain.vaillant@icm-institute.org>
2023-02-08 10:38:15 +0100

    NEW: Add task definition for FNIRT

--- pydra/tasks/fsl/__init__.py
+++ pydra/tasks/fsl/__init__.py
@@ -5,10 +5,12 @@
 
 from .bet import BET
 from .flirt import FLIRT
+from .fnirt import FNIRT
 from .fslmerge import FSLMerge
 
 __all__ = [
     "BET",
     "FLIRT",
+    "FNIRT",
     "FSLMerge",
 ]
