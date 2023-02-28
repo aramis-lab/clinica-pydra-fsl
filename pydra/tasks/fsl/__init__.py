5749bac1159fc7f93b8fd2d3fd089390a47ab70d
Ghislain Vaillant <ghislain.vaillant@icm-institute.org>
2023-02-28 20:09:06 +0100

    NEW: Add interfaces for eddy

--- pydra/tasks/fsl/__init__.py
+++ pydra/tasks/fsl/__init__.py
@@ -4,12 +4,14 @@
 """
 
 from .bet import BET
+from .eddy import Eddy
 from .flirt import FLIRT
 from .fnirt import FNIRT
 from .fslmerge import FSLMerge
 
 __all__ = [
     "BET",
+    "Eddy",
     "FLIRT",
     "FNIRT",
     "FSLMerge",
