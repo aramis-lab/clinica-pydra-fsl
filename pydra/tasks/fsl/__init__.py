ac622ede5ea6075f3466c9e231a671ebc9c4568e
Ghislain Vaillant <ghislain.vaillant@icm-institute.org>
2023-04-01 21:23:57 +0200

    REF: Move FSL utils to own subpackage

--- pydra/tasks/fsl/__init__.py
+++ pydra/tasks/fsl/__init__.py
@@ -3,13 +3,10 @@
 >>> from pydra.tasks import fsl
 """
 
-from . import fslmaths
 from .bet import BET, RobustFOV
 from .eddy import Eddy
 from .fast import FAST
 from .flirt import FLIRT, ApplyXFM, ConcatXFM, ConvertXFM, InvertXFM
 from .fnirt import FNIRT, ApplyWarp, ConvertWarp, InvWarp
-from .fslmerge import FSLMerge
-from .fslreorient2std import FSLReorient2Std
-from .fslroi import FSLROI
 from .susan import SUSAN
+from .utils import FSLROI, FSLMerge, FSLReorient2Std, fslmaths
