b1fb2fc18d35e3ae18b79c79b62dbf5c1261facc
NicolasGensollen <nicolas.gensollen@gmail.com>
2023-09-15 12:32:59 +0200

    add threshold to fsl maths

--- src/pydra/tasks/fsl/maths.py
+++ src/pydra/tasks/fsl/maths.py
@@ -20,7 +20,7 @@ Multiply input image with a binary mask:
 'fslmaths input.nii -mul mask.nii output.nii'
 """
 
-__all__ = ["Maths", "MathsSpec", "Mul"]
+__all__ = ["Maths", "MathsSpec", "Mul", "Threshold"]
 
 from os import PathLike
 
@@ -78,6 +78,21 @@ class Mul(Maths):
     """Task definition for fslmaths' mul."""
 
     input_spec = SpecInfo(name="Input", bases=(MulSpec,))
+
+
+@define(kw_only=True)
+class ThresholdSpec(MathsSpec):
+    """Specifications for fslmaths' threshold."""
+
+    threshold: float = field(
+        metadata={"help_string": "value for thresholding the image", "mandatory": True, "argstr": "-thr"}
+    )
+
+
+class Threshold(Maths):
+    """Task definition for fslmaths' threshold."""
+
+    input_spec = SpecInfo(name="Input", bases=(ThresholdSpec,))
 
 
 # TODO: Drop compatibility alias for 0.x
